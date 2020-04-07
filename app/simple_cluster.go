2fdb37dfc4699f8c899604f1a6ece029a4a9ab3c
Harold Alcala <harold.dev@gmail.com>
2020-04-07 21:53:09 +0800

    Simple Cluster

---
+++ app/simple_cluster.go
@@ -0,0 +1,89 @@
+package app
+
+import (
+	"fmt"
+
+	"github.com/mattermost/mattermost-server/v5/einterfaces"
+	"github.com/mattermost/mattermost-server/v5/model"
+
+	redis "github.com/go-redis/redis/v7"
+)
+
+type SimpleCluster struct {
+	messageHandlers map[string]*einterfaces.ClusterMessageHandler
+
+	redisClient *redis.Client
+}
+
+func NewSimpleCluster() *SimpleCluster {
+	simpleCluster := &SimpleCluster{}
+
+	simpleCluster.messageHandlers = map[string]*einterfaces.ClusterMessageHandler{}
+
+	return simpleCluster
+}
+
+func (s *SimpleCluster) StartInterNodeCommunication() {
+
+	s.redisClient = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	pong, err := s.redisClient.Ping().Result()
+
+	fmt.Println(pong, err)
+	// Output: PONG <nil>
+}
+
+func (s *SimpleCluster) StopInterNodeCommunication() {
+
+}
+
+func (s *SimpleCluster) RegisterClusterMessageHandler(event string, crm einterfaces.ClusterMessageHandler) {
+
+	fmt.Printf("event: %v, crm: %v\n", event, crm)
+
+	s.messageHandlers[event] = &crm
+}
+
+func (s *SimpleCluster) GetClusterId() string {
+	return ""
+}
+
+func (s *SimpleCluster) IsLeader() bool {
+	return true
+}
+
+func (s *SimpleCluster) GetMyClusterInfo() *model.ClusterInfo {
+	return nil
+}
+
+func (s *SimpleCluster) GetClusterInfos() []*model.ClusterInfo {
+	return []*model.ClusterInfo{}
+}
+
+func (s *SimpleCluster) SendClusterMessage(cluster *model.ClusterMessage) {
+
+}
+
+func (s *SimpleCluster) NotifyMsg(buf []byte) {
+
+}
+
+func (s *SimpleCluster) GetClusterStats() ([]*model.ClusterStats, *model.AppError) {
+	return nil, nil
+}
+
+func (s *SimpleCluster) GetLogs(page, perPage int) ([]string, *model.AppError) {
+	return []string{}, nil
+}
+
+func (s *SimpleCluster) GetPluginStatuses() (model.PluginStatuses, *model.AppError) {
+	return model.PluginStatuses{}, nil
+}
+
+func (s *SimpleCluster) ConfigChanged(previousConfig *model.Config, newConfig *model.Config, sendToOtherServer bool) *model.AppError {
+	return nil
+}
