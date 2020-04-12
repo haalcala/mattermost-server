eb83c6ff2fc5c1fa517861c2bde0565d6d7d7fe7
Harold Alcala <harold.dev@gmail.com>
2020-04-13 01:33:32 +0800

    Simple Cluster
    Master/slave role

--- app/simple_cluster.go
+++ app/simple_cluster.go
@@ -152,11 +152,17 @@ func (s *SimpleCluster) RegisterClusterMessageHandler(event string, crm einterfa
 }
 
 func (s *SimpleCluster) GetClusterId() string {
-	return ""
+	hostname, err := os.Hostname()
+
+	if err != nil {
+		panic(err)
+	}
+
+	return hostname
 }
 
 func (s *SimpleCluster) IsLeader() bool {
-	return true
+	return os.Getenv("MM_REDIS_CLUSTER_ROLE") == "master"
 }
 
 func (s *SimpleCluster) GetMyClusterInfo() *model.ClusterInfo {
